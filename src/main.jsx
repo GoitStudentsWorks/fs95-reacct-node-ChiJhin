f83c257dd1772ff492f8bbb2cfcb6622b259e334
Dimitry Minchukov <112732061+ChiJhin@users.noreply.github.com>
2024-05-09 15:59:58 +0300

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRoutr basename="/aqua_track">
+    <BrowserRouter basename="/aqua_track">
       <App />
-    </BrowserRoutr>
+    </BrowserRouter>
   </React.StrictMode>
 );
