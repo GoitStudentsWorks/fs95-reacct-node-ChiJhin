1510002866715997f54ddf3157ec0c8d73823136
Dimitry Minchukov <112732061+ChiJhin@users.noreply.github.com>
2024-05-10 17:15:39 +0300

    little fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
-import { Layout } from './components/Layout/Layout';
 import ErrorPage from 'pages/ErrorPage/ErrorPage';
 // import { useDispatch } from "react-redux";
 import { lazy } from 'react';
-import { RestrictedRoute } from './components/RestrictedRoute/RestrictedRoute.jsx';
-import { PrivateRoute } from './components/PrivateRoute/PrivateRoute.jsx';
+// import { RestrictedRoute } from './components/RestrictedRoute/RestrictedRoute.jsx';
+// import { PrivateRoute } from './components/PrivateRoute/PrivateRoute.jsx';
 // import { refreshUser } from "./redux/auth/operations.js";
 // import { useAuth } from "./hooks";
-import { AppWrapper } from './App.styled';
 
 const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
 const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage.jsx'));
@@ -16,37 +14,12 @@ const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage.jsx'));
 
 export default function App() {
   return (
-    <AppWrapper>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route
-            path="/signup"
-            element={
-              <RestrictedRoute
-                redirectTo="/tracker"
-                component={<SignUpPage />}
-              />
-            }
-          />
-          <Route
-            path="/signin"
-            element={
-              <RestrictedRoute
-                redirectTo="/tracker"
-                component={<SignInPage />}
-              />
-            }
-          />
-          <Route
-            path="/tracker"
-            element={
-              <PrivateRoute redirectTo="/" component={<TrackerPage />} />
-            }
-          />
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
-      </Routes>
-    </AppWrapper>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/signin" element={<SignInPage />} />
+      <Route path="/tracker" element={<TrackerPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
   );
 }
