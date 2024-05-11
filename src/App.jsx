a0ce2a63cec746c5d65904bc7db4bf2336d6d10d
Dimitry Minchukov <112732061+ChiJhin@users.noreply.github.com>
2024-05-11 05:32:48 +0300

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import ErrorPage from 'pages/ErrorPage/ErrorPage';
-import { lazy, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useAuth } from './hooks/useAuth.js';
 import { refresh } from './redux/auth/operations.js';
@@ -9,11 +9,10 @@ import Loader from './components/Loader/Loader.jsx';
 import Error from './components/Error/Error.jsx';
 import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute.jsx';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute.jsx';
-
-const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
-const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage.jsx'));
-const SignInPage = lazy(() => import('./pages/SignInPage/SignInPage.jsx'));
-const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage.jsx'));
+import HomePage from './pages/HomePage/HomePage.jsx';
+import SignInPage from './pages/SignInPage/SignInPage.jsx';
+import SignUpPage from './pages/SignUpPage/SignUpPage.jsx';
+import TrackerPage from './pages/TrackerPage/TrackerPage.jsx';
 
 export default function App() {
   const dispatch = useDispatch();
