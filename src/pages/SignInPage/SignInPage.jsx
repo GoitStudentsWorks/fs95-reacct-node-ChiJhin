37dec1605778634d018b6890ab1a5f4c0ab61069
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-05-13 20:56:46 +0300

    fixed WaterModal

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,11 +1,15 @@
 import SignInForm from '../../components/SignInForm/SignInForm';
 
+import WaterModal from '../../components/Modals/WaterModal/WaterModal';
+
 export default function SignInPage() {
   return (
     <main>
       <div>
         <h1>SignIn</h1>
         <SignInForm />
+
+        <WaterModal />
       </div>
     </main>
   );
