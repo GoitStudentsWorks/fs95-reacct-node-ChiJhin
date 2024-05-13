df90659818cabef01a6c7b07bd0865c0efb0c95e
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-05-13 21:08:21 +0300

    branch checked

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,15 +1,11 @@
 import SignInForm from '../../components/SignInForm/SignInForm';
 
-import WaterModal from '../../components/Modals/WaterModal/WaterModal';
-
 export default function SignInPage() {
   return (
     <main>
       <div>
         <h1>SignIn</h1>
         <SignInForm />
-
-        <WaterModal />
       </div>
     </main>
   );
