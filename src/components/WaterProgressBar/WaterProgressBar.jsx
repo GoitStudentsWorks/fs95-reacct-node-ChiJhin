c86b3cd88819f925de34908e475980381ab71008
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-05-14 20:42:50 +0300

    added selectors

--- src/components/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -1,31 +1,24 @@
-import { useState, useEffect } from 'react';
 import css from './WaterProgressBar.module.css';
 import sprite from '../../assets/sprite.svg';
+import { useSelector } from 'react-redux';
+import { selectWaterLoading } from '../../redux/water/selectors';
+// import { selectDayWater } from '../../redux/water/selectors';
+// import { selectDay } from '../../redux/water/selectors';
 
 export default function WaterProgressBar() {
-  const [waterAmount, setWaterAmount] = useState(0);
-  const [selectedDay, setSelectedDay] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      // Math.floor(Math.random() * 101);
-      const newWaterAmount = 50;
-      const newTodayAmount = 'Wednesday';
-      setWaterAmount(newWaterAmount);
-      setSelectedDay(newTodayAmount);
-    }, 5000);
-
-    return () => clearInterval(interval);
-  }, []);
-  const isLoading = false;
+  // const waterAmount = useSelector(selectDayWater);
+  const waterAmount = 50;
+  // const selectedDay = useSelector(selectDay);
+  const selectedDay = 'Wednesday';
+  const isLoading = useSelector(selectWaterLoading);
 
   return (
     <div className={css.progressBarContainer}>
       <div className={css.selectedDay}>
         {!isLoading ? (
-          <div className={css.nameBar}> Today</div>
-        ) : (
           <div className={css.nameBar}> {selectedDay}</div>
+        ) : (
+          <div className={css.nameBar}>Today</div>
         )}
       </div>
       <div className={css.progressBar}>
