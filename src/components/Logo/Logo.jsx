933bf711b12b9e3735d4d2e3049bafdf1e73c262
Valeriia295 <sokolsokol295@gmail.com>
2024-05-14 06:35:58 +0200

    Logo example

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -10,3 +10,19 @@ export default function Logo() {
     </>
   );
 }
+
+// import { Link } from 'react-router-dom';
+// import css from './Logo.module.css';
+
+// export default function Logo() {
+//   return (
+//     <>
+//       <Link to="/" className={css.logoFont}>
+//         <div className={css.content}>
+//           <p>AQUATRACK</p>
+//           <p>AQUATRACK</p>
+//         </div>
+//       </Link>
+//     </>
+//   );
+// }
