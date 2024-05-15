6cece52767a26b046d7ef8e38d1fa7b13a7184fb
Dimitry Minchukov <112732061+ChiJhin@users.noreply.github.com>
2024-05-15 18:07:35 +0300

    add style

--- src/components/WaterDailyNorma/WaterDailyNorma.jsx
+++ src/components/WaterDailyNorma/WaterDailyNorma.jsx
@@ -1,9 +1,10 @@
+import css from './WaterDailyNorma.module.css';
 export default function WaterDailyNorma() {
   return (
     <>
-      <div>
+      <div className={css.container}>
         <p>1.5L</p>
-        <p>My daily norma</p>
+        <p className={css.text}>My daily norma</p>
       </div>
     </>
   );
