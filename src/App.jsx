ea71af88519b4230d455f037851c5a532ca8d681
Dimitry Minchukov <112732061+ChiJhin@users.noreply.github.com>
2024-05-21 02:07:09 +0300

    fix 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
-// import ErrorPage from 'pages/ErrorPage/ErrorPage';
-import { lazy, useEffect } from 'react';
+import ErrorPage from 'pages/ErrorPage/ErrorPage';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useAuth } from './hooks/useAuth.js';
 import { refresh } from './redux/auth/operations.js';
@@ -9,18 +9,18 @@ import Loader from './components/Loader/Loader.jsx';
 import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute.jsx';
 
 import PrivateRoute from './components/PrivateRoute/PrivateRoute.jsx';
-// import HomePage from './pages/HomePage/HomePage.jsx';
-// import SignInPage from './pages/SignInPage/SignInPage.jsx';
-// import SignUpPage from './pages/SignUpPage/SignUpPage.jsx';
-// import TrackerPage from './pages/TrackerPage/TrackerPage.jsx';
+import HomePage from './pages/HomePage/HomePage.jsx';
+import SignInPage from './pages/SignInPage/SignInPage.jsx';
+import SignUpPage from './pages/SignUpPage/SignUpPage.jsx';
+import TrackerPage from './pages/TrackerPage/TrackerPage.jsx';
 import { store } from './redux/store.js';
 import { Toaster } from 'react-hot-toast';
 
-const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
-const SignInPage = lazy(() => import('./pages/SignInPage/SignInPage.jsx'));
-const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage.jsx'));
-const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage.jsx'));
-const ErrorPage = lazy(() => import('./pages/ErrorPage/ErrorPage.jsx'));
+// const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
+// const SignInPage = lazy(() => import('./pages/SignInPage/SignInPage.jsx'));
+// const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage.jsx'));
+// const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage.jsx'));
+// const ErrorPage = lazy(() => import('./pages/ErrorPage/ErrorPage.jsx'));
 
 export default function App() {
   const dispatch = useDispatch();
