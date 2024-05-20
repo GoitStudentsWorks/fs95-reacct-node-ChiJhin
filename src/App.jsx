fb0763c802d8ddb588f28bc68e5f15c99be414a9
Dimitry Minchukov <112732061+ChiJhin@users.noreply.github.com>
2024-05-20 15:05:12 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,25 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useAuth } from './hooks/useAuth.js';
 import { refresh } from './redux/auth/operations.js';
-import { selectLoading, selectError } from './redux/selectors.js';
+import { selectLoading } from './redux/selectors.js';
 import Loader from './components/Loader/Loader.jsx';
-import Error from './components/Error/Error.jsx';
 import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute.jsx';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute.jsx';
 import HomePage from './pages/HomePage/HomePage.jsx';
 import SignInPage from './pages/SignInPage/SignInPage.jsx';
 import SignUpPage from './pages/SignUpPage/SignUpPage.jsx';
 import TrackerPage from './pages/TrackerPage/TrackerPage.jsx';
+import { store } from './redux/store.js';
 
 export default function App() {
   const dispatch = useDispatch();
   const { isRefreshing } = useAuth();
 
   useEffect(() => {
-    dispatch(refresh());
+    store.dispatch(refresh());
   }, [dispatch]);
 
   const loading = useSelector(selectLoading);
-  const error = useSelector(selectError);
 
   return isRefreshing ? (
     <b>Refreshing user...</b>
@@ -53,16 +52,13 @@ export default function App() {
 
         <Route
           path="/tracker"
-          element={
-            <PrivateRoute redirectTo="/signin" component={<TrackerPage />} />
-          }
+          element={<PrivateRoute redirectTo="/" component={<TrackerPage />} />}
         />
 
         <Route path="*" element={<ErrorPage />} />
       </Routes>
 
       {loading && <Loader />}
-      {error && <Error />}
     </div>
   );
 }
