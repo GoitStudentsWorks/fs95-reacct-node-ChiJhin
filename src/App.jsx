e125a7e9518d94f0d1bd3a987347624bbebd9fd1
Dimitry Minchukov <112732061+ChiJhin@users.noreply.github.com>
2024-05-21 11:21:10 +0300

    Get back

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,42 @@ import { Route, Routes } from 'react-router-dom';
 import { Suspense, lazy, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { refresh } from './redux/auth/operations.js';
-import { selectLoading } from './redux/selectors.js';
+import { selectError, selectLoading } from './redux/selectors.js';
 import Loader from './components/Loader/Loader.jsx';
 import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute.jsx';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute.jsx';
+import Error from './components/Error/Error.jsx';
 import { store } from './redux/store.js';
 import { Toaster } from 'react-hot-toast';
 
-const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
-const SignInPage = lazy(() => import('./pages/SignInPage/SignInPage.jsx'));
-const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage.jsx'));
-const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage.jsx'));
-const ErrorPage = lazy(() => import('./pages/ErrorPage/ErrorPage.jsx'));
+import HomePage from './pages/HomePage/HomePage.jsx';
+import SignUpPage from './pages/SignUpPage/SignUpPage.jsx';
+import SignInPage from './pages/SignInPage/SignInPage.jsx';
+import TrackerPage from './pages/TrackerPage/TrackerPage.jsx';
+import ErrorPage from './pages/ErrorPage/ErrorPage.jsx';
+import { useAuth } from './hooks/useAuth.js';
+
+// const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
+// const SignInPage = lazy(() => import('./pages/SignInPage/SignInPage.jsx'));
+// const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage.jsx'));
+// const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage.jsx'));
+// const ErrorPage = lazy(() => import('./pages/ErrorPage/ErrorPage.jsx'));
 
 export default function App() {
   const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
 
   useEffect(() => {
     store.dispatch(refresh());
   }, [dispatch]);
 
   const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
 
-  return (
-    <Suspense fallback={<b>Refreshing user...</b>}>
-      <Toaster />
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
+    <div>
       <Routes>
         <Route
           path="/"
@@ -51,13 +62,16 @@ export default function App() {
 
         <Route
           path="/tracker"
-          element={<PrivateRoute redirectTo="/" component={<TrackerPage />} />}
+          element={
+            <PrivateRoute redirectTo="/signin" component={<TrackerPage />} />
+          }
         />
 
         <Route path="*" element={<ErrorPage />} />
       </Routes>
-
+      <Toaster />
       {loading && <Loader />}
-    </Suspense>
+      {error && <Error />}
+    </div>
   );
 }
